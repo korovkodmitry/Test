16debea1abe1dc47e5e9f27cf3e0f625cdbef9b1
Dmitry_Korovko <korovkodima81@gmail.com>
2026-03-20 22:14:57 +0300

    Переделал README.md
    Решил, что репозиторий будет про RL

--- Hello.py
+++
@@ -1,9 +0,0 @@
-def Hello(string = "Hello, Python!"):
-    '''
-    This is a simple Python function that prints a greeting message to the console.
-
-    type string: str
-    type return: None
-    '''
-    print(string)
-    print("This is a simple Python function that prints a greeting message to the console.")
